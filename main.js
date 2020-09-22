73e9a1dab59652fd241ced355ca13dee2b847c23
furage <71694701+furage@users.noreply.github.com>
2020-09-22 17:14:32 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+(function(){
+    var e = document.createElement("script");
+    e.setAttribute("src","https://code.jquery.com/jquery-1.9.1.js");
+    document.body.append(e);
+})();
+jQuery.noConflict();
+(function($) {
+    $('<link>').attr({
+        type: 'text/css',
+        rel: 'stylesheet',
+        href: 'https://furage.github.io/decoFont/rotate.css'
+    }).appendTo('head');
+})(jQuery);
